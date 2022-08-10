b4cb3e82a2aedee29322ceb2bdfbecdfe4d02234
Luis Fernando <luis.silva9902@gmail.com>
2022-08-10 19:20:08 -0300

    Added the function to delete a Moment

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MomentService } from 'src/app/services/moment.service';
 import { Moment } from 'src/app/Moment';
-import { Route, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-moment',
@@ -17,12 +18,25 @@ export class MomentComponent implements OnInit {
   faTimes = faTimes
   faEdit = faEdit
 
-  constructor(private momentService: MomentService, private route: ActivatedRoute) { }
+  constructor(
+    private momentService: MomentService, 
+    private route: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router
+  ){}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get("id"))
 
     this.momentService.getMoment(id).subscribe(item => this.moment = item.data)
+  }
+
+  async removeHandler(id: number){
+    await this.momentService.removeMoment(id).subscribe()
+
+    this.messagesService.add("Momento Excluído com sucesso!!!")
+
+    this.router.navigate(['/'])
   }
 
 }
