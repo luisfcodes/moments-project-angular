4fe12cfbdc9e093f670bdc9ece161f95f74172a2
Luis Fernando <luis.silva9902@gmail.com>
2022-08-17 22:43:45 -0300

    Adding comments, part 1

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -5,6 +5,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { MessagesService } from 'src/app/services/messages.service';
+import { Comment } from 'src/app/Comment';
+import { FormGroup, FormControl, Validators, FormGroupDirective } from '@angular/forms'; 
+import { CommentService } from 'src/app/services/comment.service';
 
 @Component({
   selector: 'app-moment',
@@ -17,18 +20,34 @@ export class MomentComponent implements OnInit {
 
   faTimes = faTimes
   faEdit = faEdit
+  
+  commentForm!: FormGroup
 
   constructor(
     private momentService: MomentService, 
     private route: ActivatedRoute,
     private messagesService: MessagesService,
-    private router: Router
+    private router: Router,
+    private commentService: CommentService
   ){}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get("id"))
 
     this.momentService.getMoment(id).subscribe(item => this.moment = item.data)
+
+    this.commentForm = new FormGroup({
+      text: new FormControl("", [Validators.required]),
+      username: new FormControl("", [Validators.required])
+    })
+  }
+
+  get text(){
+    return this.commentForm.get("text")
+  }
+
+  get username(){
+    return this.commentForm.get("username")
   }
 
   async removeHandler(id: number){
